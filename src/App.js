ee358aa67195d73bc25a62fa4f3a126be252b109
Akshit Arora <akshitarora9211@gmail.com>
2020-08-14 13:27:24 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my Portfolio</h1>
+      {/* <h1>This is my Portfolio</h1> */}
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+import "../css/header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="heading-background">AKSHIT</h1>
+      <header>
+        <h1>
+          <Fade bottom cascade>
+            Akshit Arora
+          </Fade>
+        </h1>
+      </header>
+      <Fade bottom>
+        <p className="header-title">
+          Hi <span role="img">👋</span> You found me! I am Akshit.
+          <br />
+          Software Engineer
+          <br />
+          from India
+          <br />
+          <button>
+            <a
+              href="mailto:akshitarora9211@gmail.com"
+              rel="noopener noreferrer"
+            >
+              Contact
+            </a>
+          </button>
+        </p>
+      </Fade>
+    </div>
+  );
+}
+
+export default Header;
